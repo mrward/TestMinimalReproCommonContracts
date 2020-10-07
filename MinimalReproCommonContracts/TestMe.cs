4f656983d6193a7e5b38c6f8cc65acd987b33383
Matt Ward <matt.ward@microsoft.com>
2020-10-07 15:00:53 +0100

    Add repro

---
+++ MinimalReproCommonContracts/TestMe.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MinimalReproCommonContracts
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TestMe
+    {
+        /// <summary>
+        /// Unknown.
+        /// </summary>
+        Unknown = 0,
+    }
+}
